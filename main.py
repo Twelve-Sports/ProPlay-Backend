ae53d2a4e0f0aaef3a6ad57944bfef0326b61b8c
ThorGalli <gallithor@gmail.com>
2023-11-22 11:35:30 -0300

    add comments and blank post function

--- main.py
+++ main.py
@@ -16,12 +16,15 @@ os.makedirs(RECORDINGS_DIRECTORY, exist_ok=True)
 
 cam_directories = list(map(lambda x: os.path.join(RECORDINGS_DIRECTORY, x), os.listdir(RECORDINGS_DIRECTORY)))
 
-def get_timestamps(directory_name):
+def get_timestamps_from_api(directory_name):
     directory_name = os.path.basename(directory_name)
     response = requests.get(f'http://192.168.251.146:3001/list/{directory_name}').json()
     # {'data': '2023-11-22T09:58:34.427Z', 'id': 24, 'court_name': 'Quadra 8', 'court_id': 8}
     timestamps = list(map(lambda x: x['data'], response))
     return timestamps
+
+def post_clip_to_api(directory_name, timestamp):
+    return ''
 
 def is_timestamp_in_video(clip_datetime, video_start_datetime):
     # verify if the timestamp is within the video duration
@@ -30,7 +33,7 @@ def is_timestamp_in_video(clip_datetime, video_start_datetime):
 
 for directory in cam_directories:
     directory_name = os.path.basename(directory)
-    timestamps = get_timestamps(directory_name)
+    timestamps = get_timestamps_from_api(directory_name)
     for root, directories, files in os.walk(directory):
         for file in files:
             # Check if the file is a video file
@@ -68,53 +71,8 @@ for directory in cam_directories:
 
                     # Check if the input video file exists before creating the clip
                     if os.path.exists(input_video_file):
+                        # FIX ME: The clip is sometimes created with a longer duration than the expected
+                        # With the extra duration being only black screen
                         ffmpeg_extract_subclip(input_video_file, crop_start, crop_end, targetname=output_clip)
                     else:
                         print(f"Input video file '{input_video_file}' does not exist.")
-
-            
-
-
-
-# for root, _, files in os.walk():
-#     for file in files:
-#         is_timestamp_file = file.startswith("timestamp_") and file.endswith(".xml")
-#         if is_timestamp_file:   
-
-#             # Get the timestamp value from the XML file            
-#             timestamp_path = os.path.join(root, file)
-#             timestamp_tree = ET.ElementTree(file=timestamp_path)
-#             timestamp_root = timestamp_tree.getroot()
-#             timestamp_value = timestamp_root.find('value').text
-
-#             # Calculate the start and end timestamps for the clip
-#             clip_end_timestamp = datetime.datetime.strptime(timestamp_value, "%Y-%m-%d %H-%M-%S-%f")
-#             clip_start_timestamp = clip_end_timestamp - datetime.timedelta(seconds=CLIP_DURATION)
-
-#             # Calculate the start and end values relative to the original video
-#             clip_start_relative = clip_start_timestamp - datetime.datetime.strptime(recording_timestamp, "%Y-%m-%d %H-%M-%S-%f")
-#             clip_end_relative = clip_end_timestamp - datetime.datetime.strptime(recording_timestamp, "%Y-%m-%d %H-%M-%S-%f")
-
-#             # Convert the relative values to real numbers
-#             crop_start = max(0, clip_start_relative.total_seconds())
-#             crop_end = clip_end_relative.total_seconds()
-            
-#             # Define the input and output file paths for the clip
-#             input_video_file = os.path.join(root, f"video_{timestamp_value}.avi")
-#             output_clip = os.path.join(clips_directory, f"clip_{timestamp_value}.avi")              
-
-#             # Check if the input video file exists before creating the clip
-#             if os.path.exists(filename):
-#                 ffmpeg_extract_subclip(filename, crop_start, crop_end, targetname=output_clip)
-#             else:
-#                 print(f"Input video file '{filename}' does not exist.")
-
-# # Move all timestamps to a separate directory
-# timestamps_directory = os.path.join(video_directory, 'timestamps')
-# os.makedirs(timestamps_directory, exist_ok=True)
-# for root, _, files in os.walk(video_directory):
-#     for file in files:
-#         if file.startswith("timestamp_") and file.endswith(".xml"):
-#             os.rename(os.path.join(root, file), os.path.join(timestamps_directory, file))
-
-
